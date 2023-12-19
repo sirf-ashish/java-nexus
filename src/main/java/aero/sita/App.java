40422163afaf6ed73826898c12cce698a8fd27ce
root <root@nexuslab.localdomain>
2023-12-20 00:45:05 +0530

    First Commit

---
+++ src/main/java/aero/sita/App.java
@@ -0,0 +1,13 @@
+package aero.sita;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/aero/sita/AppTest.java
@@ -0,0 +1,20 @@
+package aero.sita;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
